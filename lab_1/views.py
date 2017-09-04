27fcf259d258818726d0efa5e06ba7846b05575a
Sayyid <hafiyyan94@gmail.com>
2017-09-04 10:46:44 +0700

    Reducing variable in lab_1

--- lab_1/views.py
+++ lab_1/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
 from datetime import datetime, date
 # Enter your name here
-mhs_name = 'Hafiyyan' # TODO Implement this
+mhs_name = '' # TODO Implement this
 curr_year = int(datetime.now().strftime("%Y"))
-birth_date = date(1994,8,6) #TODO Implement this, format (Year, Month, Date)
+birth_date = date() #TODO Implement this, format (Year, Month, Date)
 # Create your views here.
 def index(request):
     response = {'name': mhs_name, 'age': calculate_age(birth_date.year)}
     return render(request, 'index_lab1.html', response)
-
 
 def calculate_age(birth_year):
     return curr_year - birth_year if birth_year <= curr_year else 0
